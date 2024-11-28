eff1568c3472f1c291a1f9a0516bdea3ff994fd1
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-11-28 14:33:10 -0500

    Add token expiry of 1 hour

--- src/AntiCsrf.php
+++ src/AntiCsrf.php
@@ -17,17 +17,20 @@ class AntiCsrf
     {
         $this->startSession();
         $token = bin2hex(random_bytes(32));
-        $_SESSION['SpCsrfToken'] = $token;
+        $_SESSION['SpCsrfToken'] = [
+            'value' => $token,
+            'expiry' => time() + 3600 // Token expires in 1 hour
+        ];
         return $token;
     }
 
     public function tokenIsValid(string $tokenToCheck): bool
     {
         $this->startSession();
-        if (empty($_SESSION['SpCsrfToken'])) {
+        if (empty($_SESSION['SpCsrfToken']) || time() > $_SESSION['SpCsrfToken']['expiry']) {
             return false;
         }
-        $isValid = hash_equals($_SESSION['SpCsrfToken'], $tokenToCheck);
+        $isValid = hash_equals($_SESSION['SpCsrfToken']['value'], $tokenToCheck);
         if ($isValid) {
             unset($_SESSION['SpCsrfToken']);
         }
