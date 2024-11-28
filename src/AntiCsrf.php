71d5ce5a4f60cee6950da5d57f3687a180a9eb51
Spencer14420 <67592325+Spencer14420@users.noreply.github.com>
2024-11-28 14:26:19 -0500

    Implement strict typing

--- src/AntiCsrf.php
+++ src/AntiCsrf.php
@@ -1,27 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace spencer14420\SpAntiCsrf;
 
 class AntiCsrf
 {
-    private function startSession()
+    private function startSession(): void
     {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
     }
 
-    //Sets and returns a "SpCsrfToken" session variable
-    public function generateToken()
+    // Sets and returns a "SpCsrfToken" session variable
+    public function generateToken(): string
     {
         $this->startSession();
-        $token
-            = bin2hex(random_bytes(32));
+        $token = bin2hex(random_bytes(32));
         $_SESSION['SpCsrfToken'] = $token;
         return $token;
     }
 
-    public function tokenIsValid($tokenToCheck)
+    public function tokenIsValid(string $tokenToCheck): bool
     {
         $this->startSession();
         if (empty($_SESSION['SpCsrfToken'])) {
